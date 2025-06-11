7833f3fba6cda7d80b9d720d00c2b6a34fa44deb
bonkisss <karpova.kati0315@gmail.com>
2025-06-11 12:02:14 +0300

    feat: cards
    - create card
    - delete card
    - add card list

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, handleDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => handleDelete(cardElement));
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+  const cardElement = createCard(cardData, handleDeleteCard);
+  placesList.appendChild(cardElement);
+});
