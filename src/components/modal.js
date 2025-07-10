c7fd7700957a82db41e93860e5c35a185cc8938c
bonkisss <karpova.kati0315@gmail.com>
2025-07-10 13:12:11 +0300

    feat: improve modal display and animation handling

--- src/components/modal.js
+++ src/components/modal.js
@@ -8,30 +8,11 @@ function closeEscPopup(evt) {
 }
 
 export function openModal(modal) {
-  modal.classList.remove('popup_is-animated');
   modal.classList.add('popup_is-opened');
-  // Сброс opacity, чтобы transition сработал
-  modal.style.opacity = '0';
-  modal.style.display = 'flex';
-  modal.style.visibility = 'visible';
-  modal.style.pointerEvents = 'all';
   document.addEventListener('keydown', closeEscPopup);
-
-  // Запускаем анимацию появления
-  requestAnimationFrame(() => {
-    modal.style.opacity = '1';
-  });
 }
 
 export function closeModal(modal) {
-  // Плавно скрываем
-  modal.style.opacity = '0';
-  modal.style.pointerEvents = 'none';
-  modal.style.visibility = 'hidden';
+  modal.classList.remove('popup_is-opened');
   document.removeEventListener('keydown', closeEscPopup);
-
-  setTimeout(() => {
-    modal.classList.remove('popup_is-opened');
-    modal.style.display = 'none';
-  }, 600); // 600ms = время transition
 }
